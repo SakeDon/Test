fad91d4e082f7a834fbd7624c201644476d82c8d
SakeDon <wngks1122@gmail.com>
2020-09-16 10:56:41 +0900

    hehe

---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+int main(int argc, char** argv){
+
+	srand(time(NULL));
+	printf("%d\n",rand()%100);
+	printf("%d\n",rand()%100);
+	printf("%d\n",rand()%100);
+	printf("%d\n",rand()%100);
+	printf("%d\n",rand()%100);
+	printf("%d\n",rand()%100);
+	printf("%d\n",rand()%100);
+
+	return 0;
+}
